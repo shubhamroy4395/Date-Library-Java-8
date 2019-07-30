b47cbbd0ab8efb3a2f92710a81ac6b6b73d3c1e8
Shubham Roy <shubhamroy4395@gmail.com>
2019-07-30 13:52:00 +0530

    Updated Junit

--- src/dateToLocalDate/DateLocalDateUTest.java
+++ src/dateToLocalDate/DateLocalDateUTest.java
@@ -20,4 +20,9 @@ public class DateLocalDateUTest {
 	public void testConvertDate() {
 		assertEquals("29-07-2019",DateLocalDate.convertDate("2019-07-29","yyyy-MM-dd","dd-MM-yyyy"));
 	}
+	
+	@Test
+	public void testDateToLocalDateTime() {
+		assertEquals(LocalDateTime.now().withNano(0).withSecond(0),DateLocalDate.dateToLocalDateTime(date).withNano(0).withSecond(0));
+	}
 }
