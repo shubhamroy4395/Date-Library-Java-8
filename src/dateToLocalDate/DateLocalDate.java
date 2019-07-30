bedd66e9a92896a3f8dae812379c72e71fcd1359
Shubham Roy <shubhamroy4395@gmail.com>
2019-07-30 10:23:12 +0530

    Main method removed

--- src/dateToLocalDate/DateLocalDate.java
+++ src/dateToLocalDate/DateLocalDate.java
@@ -14,18 +14,6 @@ import javax.xml.datatype.XMLGregorianCalendar;
 
 public final class DateLocalDate {
 	
-
-
-	public static void main(String[] args) {
-		Date date1 = new Date();
-		XMLGregorianCalendar date = getXMLGregorianCalendar("2008-05-21 15:35","yyyy-MM-dd HH:mm");
-		System.out.println(date);
-		System.out.println("local date is "+dateToLocalDate(date1));
-		System.out.println(getTimeToLocalDate(date1.getTime()));
-		System.out.println(convertDate("2019-07-29","yyyy-MM-dd","dd-MM-yyyy"));
-
-	}
-	
 	//convert date to localdate
 	public static LocalDate dateToLocalDate(Date date) {
 		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
